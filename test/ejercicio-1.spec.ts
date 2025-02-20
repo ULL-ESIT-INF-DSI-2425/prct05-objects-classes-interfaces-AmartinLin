97b24892603082eb5ba3c77f7756f916606fe69c
Alejandro M. Linares <alu0101476476@ull.edu.es>
2025-02-20 16:08:57 +0000

    Test adición de pokemon nuevo

--- test/ejercicio-1.spec.ts
+++ test/ejercicio-1.spec.ts
@@ -28,4 +28,9 @@ describe("Clase Pokedex", () => {
   test("Filtrado, por valor, ataque > 50", () => {
     expect(miPokedex.filtrarPorCondicion("Ataque", (ataque) => ataque > 50)).toStrictEqual([Pikachu, Charmander]);
   });
+  
+  test("adición de un nuevo pokemon", () => {
+    miPokedex.agregarPokemon({nombre : "Flareon", Peso : 30, Altura: 1.0, Tipo: "Fuego", Ataque: 70, Defensa: 40, Velocidad: 30, HP: 70})
+    expect(miPokedex.filtrarPorAtributo("nombre", "Flareon")).toStrictEqual([{nombre : "Flareon", Peso : 30, Altura: 1.0, Tipo: "Fuego", Ataque: 70, Defensa: 40, Velocidad: 30, HP: 70}])
+  })
 });
