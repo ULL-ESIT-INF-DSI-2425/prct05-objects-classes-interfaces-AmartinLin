389e8473e862d4ec2c2e6c17a00d4a886c98fdd6
Alejandro M. Linares <alu0101476476@ull.edu.es>
2025-02-21 11:53:35 +0000

    test disco

---
+++ test/ejercicio-2.spec.ts
@@ -0,0 +1,36 @@
+import { describe, expect, test } from "vitest";
+import { Artista } from "../src/ejercicio-2/Artista";
+import { Disco } from "../src/ejercicio-2/Disco";
+//import { Cancion } from "../src/ejercicio-2/Cancion";
+import { biblioteca } from "../src/ejercicio-2/biblioteca";
+
+// describe("Clase Canción", () => {
+//   const cancion : Cancion = {nombre: "Crossroads", tiempo: 314, genero: "Blues Rock", single: false, reproducciones: 810465};
+//   test("", () => {
+//     expect().toStrictEqual()
+//   })
+// });
+
+describe("Clase Disco", () => {
+  const cancion : Cancion = {nombre: "Crossroads", tiempo: 314, genero: "Blues Rock", single: false, reproducciones: 810465};
+  const disco : Disco = {nombre: "Crossroads", salida: 1988, canciones: [cancion]};
+
+  const laylaSong : Cancion = {nombre: "Layla", tiempo: 425, genero: "Blues Rock", single: false, reproducciones: 56832104};
+  const tellTheTruth : Cancion = {nombre: "Tell the truth", tiempo: 399, genero: "Blues Rock", single: false, reproducciones: 264145};
+  const layla : Disco = {nombre: "Layla and Other Assorted Love Songs", salida: 1970, canciones: [laylaSong, tellTheTruth]};
+
+  test("Calcular número de canciones del disco", () => {
+    expect(disco.nSongs()).toBe(1);
+    expect(layla.nSongs()).toBe(2);
+  })
+
+  test("Calcular tiempo de duración del disco en segundos", () => {
+    expect(disco.time()).toBe(314);
+    expect(layla.time()).toBe(399 + 425);
+  })
+
+  test("Calcular el número de reproducciones del disco", () => {
+    expect(disco.reproducciones()).toBe(810465);
+    expect(layla.reproducciones()).toBe(56832104 + 264145);
+  })
+});
