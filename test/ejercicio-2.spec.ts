2286447d40bdf23054c84913493bdbfcfdcd0208
Alejandro M. Linares <alu0101476476@ull.edu.es>
2025-02-21 13:30:45 +0000

    edicion de test

--- test/ejercicio-2.spec.ts
+++ test/ejercicio-2.spec.ts
@@ -1,15 +1,8 @@
 import { describe, expect, test } from "vitest";
-//import { Artista } from "../src/ejercicio-2/Artista";
+import { Artista } from "../src/ejercicio-2/Artista";
 import { Disco } from "../src/ejercicio-2/Disco";
 import { Cancion } from "../src/ejercicio-2/Cancion";
 //import { Biblioteca } from "../src/ejercicio-2/Biblioteca";
-
-// describe("Clase Canción", () => {
-//   const cancion : Cancion = {nombre: "Crossroads", tiempo: 314, genero: "Blues Rock", single: false, reproducciones: 810465};
-//   test("", () => {
-//     expect().toStrictEqual()
-//   })
-// });
 
 describe("Clase Disco", () => {
   const cancion : Cancion = {nombre: "Crossroads", tiempo: 314, genero: "Blues Rock", single: false, reproducciones: 810465};
@@ -33,4 +26,18 @@ describe("Clase Disco", () => {
     expect(disco.reproducciones()).toBe(810465);
     expect(layla.reproducciones()).toBe(56832104 + 264145);
   })
+});
+
+describe("Clase Artista", () => {
+  const cancion: Cancion = {nombre: "Crossroads", tiempo: 314, genero: "Blues Rock", single: false, reproducciones: 810465};
+  const disco: Disco = new Disco ("Crossroads", 1988, [cancion]);
+  const laylaSong: Cancion = {nombre: "Layla", tiempo: 425, genero: "Blues Rock", single: false, reproducciones: 56832104};
+  const tellTheTruth: Cancion = {nombre: "Tell the truth", tiempo: 399, genero: "Blues Rock", single: false, reproducciones: 264145};
+  const layla: Disco = new Disco("Layla and Other Assorted Love Songs", 1970, [laylaSong, tellTheTruth]);
+  const EricClapton: Artista = new Artista("Eric Clapton", 3000000, [disco, layla]);
+  test("Saber los datos del artista", () => {
+    expect(EricClapton.discos).toStrictEqual([disco, layla])
+    // Lo ponemos así ya que nos da igual el orden en que nos de las canciones por ahora
+    expect(EricClapton.canciones()).toEqual(expect.arrayContaining([laylaSong, tellTheTruth, cancion]));
+  });
 });
