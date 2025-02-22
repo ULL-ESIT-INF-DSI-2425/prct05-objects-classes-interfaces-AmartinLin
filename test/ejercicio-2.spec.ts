a7c7f8bb454f654aed16e8a5787cd6afcbceecf8
Alejandro M. Linares <alu0101476476@ull.edu.es>
2025-02-22 11:06:10 +0000

    constantes para test

--- test/ejercicio-2.spec.ts
+++ test/ejercicio-2.spec.ts
@@ -2,16 +2,29 @@ import { describe, expect, test } from "vitest";
 import { Artista } from "../src/ejercicio-2/Artista";
 import { Disco } from "../src/ejercicio-2/Disco";
 import { Cancion } from "../src/ejercicio-2/Cancion";
-//import { Biblioteca } from "../src/ejercicio-2/Biblioteca";
+import { Biblioteca } from "../src/ejercicio-2/Biblioteca";
+
+// Eric Clapton
+const cancion : Cancion = {nombre: "Crossroads", tiempo: 314, genero: "Blues Rock", single: false, reproducciones: 810465};
+const disco : Disco = new Disco ("Crossroads", 1988, [cancion]);
+const laylaSong : Cancion = {nombre: "Layla", tiempo: 425, genero: "Blues Rock", single: false, reproducciones: 56832104};
+const tellTheTruth : Cancion = {nombre: "Tell the truth", tiempo: 399, genero: "Blues Rock", single: false, reproducciones: 264145};
+const layla : Disco = new Disco("Layla and Other Assorted Love Songs", 1970, [laylaSong, tellTheTruth]);
+const EricClapton: Artista = new Artista("Eric Clapton", 3000000, [disco, layla]);
+// Taylor Swift
+const backToDecember: Cancion = {nombre: "Back to December", tiempo: 304, genero: "Pop", single: false, reproducciones: 349159746};
+const enchanted: Cancion = {nombre: "Enchanted", tiempo: 304, genero: "Pop", single: false, reproducciones: 60987447};
+const mine: Cancion = {nombre: "Mine", tiempo: 211, genero: "Pop", single: false, reproducciones: 324175705};
+const speakKnow: Disco = new Disco("Speak Know", 2010, [backToDecember, enchanted, mine]);
+const youBelongToMe: Cancion = {nombre: "You Belong to Me", tiempo: 210, genero: "Country Pop", single: false, reproducciones: 41447782};
+const loveStory: Cancion = {nombre: "Love Story", tiempo: 241, genero: "Country Pop", single: false, reproducciones: 52958553};
+const fearless: Disco = new Disco("Fearless", 2021, [youBelongToMe, loveStory]);
+const cruelSummer: Cancion = {nombre: "Cruel Summer", tiempo: 234, genero: "Country Pop" , single: false, reproducciones: 228501899};
+const theArcher: Cancion = {nombre: "The Archer", tiempo: 300, genero: "Country Pop" , single: false, reproducciones: 41287853};
+const lover: Disco = new Disco("Lover", 2019, [cruelSummer, theArcher]);
+const TaylorSwift: Artista = new Artista("Taylor Swift", 88200000, [speakKnow, fearless, lover]);
 
 describe("Clase Disco", () => {
-  const cancion : Cancion = {nombre: "Crossroads", tiempo: 314, genero: "Blues Rock", single: false, reproducciones: 810465};
-  const disco : Disco = new Disco ("Crossroads", 1988, [cancion]);
-
-  const laylaSong : Cancion = {nombre: "Layla", tiempo: 425, genero: "Blues Rock", single: false, reproducciones: 56832104};
-  const tellTheTruth : Cancion = {nombre: "Tell the truth", tiempo: 399, genero: "Blues Rock", single: false, reproducciones: 264145};
-  const layla : Disco = new Disco("Layla and Other Assorted Love Songs", 1970, [laylaSong, tellTheTruth]);
-
   test("Calcular número de canciones del disco", () => {
     expect(disco.nSongs()).toBe(1);
     expect(layla.nSongs()).toBe(2);
@@ -29,15 +42,14 @@ describe("Clase Disco", () => {
 });
 
 describe("Clase Artista", () => {
-  const cancion: Cancion = {nombre: "Crossroads", tiempo: 314, genero: "Blues Rock", single: false, reproducciones: 810465};
-  const disco: Disco = new Disco ("Crossroads", 1988, [cancion]);
-  const laylaSong: Cancion = {nombre: "Layla", tiempo: 425, genero: "Blues Rock", single: false, reproducciones: 56832104};
-  const tellTheTruth: Cancion = {nombre: "Tell the truth", tiempo: 399, genero: "Blues Rock", single: false, reproducciones: 264145};
-  const layla: Disco = new Disco("Layla and Other Assorted Love Songs", 1970, [laylaSong, tellTheTruth]);
-  const EricClapton: Artista = new Artista("Eric Clapton", 3000000, [disco, layla]);
   test("Saber los datos del artista", () => {
     expect(EricClapton.discos).toStrictEqual([disco, layla])
     // Lo ponemos así ya que nos da igual el orden en que nos de las canciones por ahora
     expect(EricClapton.canciones()).toEqual(expect.arrayContaining([laylaSong, tellTheTruth, cancion]));
   });
 });
+
+describe("Clase Biblioteca", () => {
+  const miBiblioteca: Biblioteca = new Biblioteca(EricClapton, TaylorSwift)
+  expect(miBiblioteca.info()).toBe()
+})
